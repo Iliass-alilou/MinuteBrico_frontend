27f6108c5d132cea30533212c0255c21cb9fcbf5
iliass alilou <iliassalilou18@gmail.com>
2021-04-26 15:23:31 +0000

    continue on form brico

--- src/componants/auth/signUpEtape2.js
+++ src/componants/auth/signUpEtape2.js
@@ -3,43 +3,48 @@ import InputComp from  "../shared/inputComp"
 import {Link} from "react-router-dom"
 import './style.css';
 export default class SignUpEtape2 extends Component {
+ 
+
   render() {
     return (
       <div id="signupbrico" className="fluid-container" >
         <img alt="..." src="/images/12.jpeg" className="bg" />
-            <div  className="container mt-lg-5 mt-md-5">
-              <p className="title">Devenir notre MinuteBricoleur en quelques étapes</p>
-              
-              {/*<input type="text" placeholder="Nom d'affichage" required="champs requis" />              
-              <div>
-              <input type="text" placeholder="Pay" required="champs requis"/>
-              <input type="text" placeholder="Ville" required="champs requis" />
-              </div>             
-              <div>
-                <select class="select" multiple>
-                  <option value="1">Arabe</option>
-                  <option value="2">Aglais</option>
-                  <option value="3">Francais</option>
-                </select>
-                <label class="form-label select-label">Langues </label>
-              </div>*/}
-
+            <div   className="container">
+              <p className="title">Devenir notre MinuteBricoleur<br/> en quelques étapes</p>
                 <form>
-                  {/*<!-- Name input -->*/}
-                  <div className="form-outline mb-4">
-                    <input type="text" id="form5Example1" className="form-control" />
-                    <label className="form-label" htmlFor="form5Example1">Name</label>
+                  
+                  <div className="form-outline mb-4 mt-md-5">
+                    <label className="form-label" htmlFor="form5Example1">Nom d'affichage <span style={{color:'red'}}>*</span> </label>
+                    <input type="text" id="form5Example1" className="form-control" required />
                   </div>
 
-                  {/*<!-- Email input -->*/}
+                  
                   <div className="form-outline mb-4">
-                    <input type="email" id="form5Example2" className="form-control" />
-                    <label className="form-label" htmlFor="form5Example2">Email address</label>
+                    <label className="form-label" htmlFor="form5Example2">Ville<span style={{color:'red'}}>*</span></label>
+                    <input type="email" id="form5Example2" className="form-control" required/>
+
+                    <label className="form-label" htmlFor="form5Example2">Pays<span style={{color:'red'}}>*</span></label>
+                    <input type="email" id="form5Example2" className="form-control" required/>
+                    
                   </div>
 
-                 
-                  {/*<!-- Submit button -->*/}
-                  <button type="submit" className="btn btn-primary btn-block mb-4">Suivant</button>
+                  <div className="form-outline mb-4">
+                    <label className="form-label" htmlFor="form5Example1">Langues<span style={{color:'red'}}>*</span></label>
+                    <input type="text" id="form5Example1" className="form-control" required />
+                  </div>
+
+                
+                  
+                  
+
+                  <div className="form-outline mb-4">
+                    <label class="form-label" for="customFile">Photo de profil</label>
+                    <input type="file" class="form-control" id="customFile" />
+                    <p>Veuillez importer une photo de vous, préférablement en portrait<br/><span style={{color:'red'}}>Formats supportés: jpeg, jpg, png moins de 2Mo</span></p>
+                  </div>
+
+                  <button style={{float:'left'}} type="submit" className="btn btn-primary btn-block mb-4">Précédent</button>
+                  <button style={{float:'right'}} type="submit" className="btn btn-primary btn-block mb-4">Suivant</button>
                 </form>             
             </div>
       </div>
